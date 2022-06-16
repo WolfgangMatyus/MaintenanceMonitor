b2a994ee4f2b4e3bc0626409c4fc668999592b26
Matyus Wolfgang <wi21b055@technikum-wien.at>
2022-06-16 13:51:21 +0200

    Initial commit

---
+++ src/main/java/at/msw/MaintenanceMonitor/MaintenanceMonitor.java
@@ -0,0 +1,13 @@
+package at.msw.MaintenanceMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitor {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitor.class, args);
+    }
+
+}
---
+++ src/test/java/at/msw/MaintenanceMonitor/MaintenanceMonitorTests.java
@@ -0,0 +1,13 @@
+package at.msw.MaintenanceMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
